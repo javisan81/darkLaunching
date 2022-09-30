1a5b3b7bb9361c30b0cb959ed812e1aed6ddb2a9
Javier Lopez Fernandez <javierlopezfernandez@MacBook-Pro-de-Javier.local>
2022-09-30 17:43:48 +0200

    refactor

--- src/main/java/com/company/GovermentMessageRepository.java
+++ src/main/java/com/company/GovermentMessageRepository.java
@@ -5,6 +5,4 @@ import java.math.BigDecimal;
 public interface GovermentMessageRepository {
     void sendToGoverment(String product, BigDecimal vatCalculated);
 
-    String getProduct();
-    BigDecimal getVatCalculated();
 }
--- src/test/java/com/company/VatTest.java
+++ src/test/java/com/company/VatTest.java
@@ -7,65 +7,38 @@ import java.math.BigDecimal;
 import static org.junit.Assert.assertEquals;
 
 public class VatTest {
-    private final GovermentMessageRepository inMemoryGovernmentRepository = new GovermentMessageRepository() {
-        public BigDecimal vatCalculated;
-        public String product;
-        @Override
-        public void sendToGoverment(String product, BigDecimal vatCalculated) {
-            this.vatCalculated=vatCalculated;
-            this.product=product;
-        }
-
-        @Override
-        public String getProduct() {
-            return product;
-        }
-
-        @Override
-        public BigDecimal getVatCalculated() {
-            return vatCalculated;
-        }
+    private static BigDecimal vatCalculated;
+    private static String product;
+    private final GovermentMessageRepository inMemoryGovernmentRepository = (product, vatCalculated) -> {
+        VatTest.vatCalculated = vatCalculated;
+        VatTest.product = product;
     };
-    private final GovermentMessageRepository nullGovernmentRepository = new GovermentMessageRepository() {
-        @Override
-        public void sendToGoverment(String product, BigDecimal vatCalculated) {
-
-        }
-
-        @Override
-        public String getProduct() {
-            return null;
-        }
-
-        @Override
-        public BigDecimal getVatCalculated() {
-            return null;
-        }
-    };
+    private final GovermentMessageRepository nullGovernmentRepository = (product, vatCalculated) -> {};
 
     @Test
-    public void forCharity(){
+    public void forCharity() {
         Vat vat = new Vat(() -> false, nullGovernmentRepository);
         assertEquals(BigDecimal.ZERO, vat.calculate("charity"));
         assertEquals(BigDecimal.ZERO, vat.calculate("charity"));
     }
 
     @Test
-    public void forBread(){
+    public void forBread() {
         Vat vat = new Vat(() -> false, inMemoryGovernmentRepository);
         assertEquals(BigDecimal.valueOf(4), vat.calculate("bread"));
-        assertEquals(BigDecimal.valueOf(10), inMemoryGovernmentRepository.getVatCalculated());
-        assertEquals("bread", inMemoryGovernmentRepository.getProduct());
+        assertEquals(BigDecimal.valueOf(10), vatCalculated);
+        assertEquals("bread", product);
     }
+
     @Test
-    public void newVatForCharity(){
-        ToggleRepository toggleRepository= () -> true;
+    public void newVatForCharity() {
+        ToggleRepository toggleRepository = () -> true;
         Vat vat = new Vat(toggleRepository, inMemoryGovernmentRepository);
         assertEquals(BigDecimal.valueOf(5), vat.calculate("charity"));
     }
 
     @Test
-    public void newVatForBread(){
+    public void newVatForBread() {
         Vat vat = new Vat(() -> true, nullGovernmentRepository);
         assertEquals(BigDecimal.valueOf(10), vat.calculate("bread"));
     }
